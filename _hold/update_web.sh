e46c72d145f0372e222be22fd288a15f124a71bb
Iago Mosquera <flackon@gmail.com>
2013-09-15 14:15:18 +0200

    Enthusia

--- _hold/update_web.sh
+++ _hold/update_web.sh
@@ -15,4 +15,4 @@ USER="iacus"
 #lftp -e "'mirror -R -v $ORIGDIR . ; bye' $URL"
 #lftp -e 'mirror -R -v --only-newer /mnt/whome/data/web/guayaba-ng/_site . ; bye' $URL
 
-rsync -avr -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete --delete-after --progress --log-file=$LOGDIR/rsync-guayaba.log $ORIGDIR $USER@$URL:/var/www/
+rsync -avr -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete --delete-after --progress --log-file=$LOGDIR/rsync-guayaba.log $ORIGDIR $USER@$URL:/var/www/guayaba2600.com/
