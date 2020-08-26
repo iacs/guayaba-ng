e21f79ceafc3b69d0b62e8611990583c2df08514
Iago Mosquera <flackon@gmail.com>
2020-08-27 00:15:37 +0200

    Software de la web actualizado
    Actualizado a Jekyll 4.1.1
    Actualizado a Bootstrap 4.5.2
    Añadido Octicons
    Migrado filtro de jQuery a JS
    Eliminado Compass
    Eliminado tareas obsoletas
    Cambiado Enlaces

--- js/filter.js
+++ js/filter.js
@@ -1,7 +1,18 @@
-$('input.filter').on('keyup', function() {
-    var rex = new RegExp($(this).val(), 'i');
-    $('.searchable tr').hide();
-    $('.searchable tr').filter(function() {
-        return rex.test($(this).text());
-    }).show();
-});
+function filterName() {
+    var box = document.querySelector('input.filter');
+    var filterStr = box.value.toUpperCase();
+    var table = document.querySelector('table');
+    var trs = table.getElementsByTagName('tr');
+
+    for (tr of trs) {
+        td = tr.getElementsByTagName('td')[0];
+        if (td) {
+            let txtval = td.textContent || td.innerText;
+            if (txtval.toUpperCase().indexOf(filterStr) > -1) {
+                tr.style.display = '';
+            } else {
+                tr.style.display = 'none';
+            }
+        }
+    };
+}
